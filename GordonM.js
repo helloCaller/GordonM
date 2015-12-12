ea40000da89a34959377f1b525ecd748a04dd8e1
Luke Garwood <luke_gar@hotmail.com>
2015-12-11 21:43:31 -0500

    preliminary publish and subscribe

--- GordonM.js
+++ GordonM.js
@@ -1,53 +1,58 @@
 var pubnub = PUBNUB({
     subscribe_key: 'sub-c-03f3177c-9c7e-11e5-b829-02ee2ddab7fe', // always required
+    publish_key: 'pub-c-8c19144e-3d5a-4e5b-8442-f56d8721b99c'    // only required if publishing
 });
-
+var Flashcolor;
 pubnub.subscribe({
     channel: 'GMflash',
-    message: function(m){console.log(m)},
+    message: function(m){document.getElementById("container").style.backgroundColor = m.color},
     error: function (error) {
       // Handle error here
       console.log(JSON.stringify(error));
     }
  });
 
+function publish() {
+pubnub.publish({
+    channel: 'GMflash',        
+    message: flashcontrol = {
+                color: "red"}
+    ,
+    callback : function(m){console.log("publish " + flashcontrol.color)}
+});
+}
 
 
 
 
 
-if ((window.DeviceMotionEvent) {
-  window.addEventListener('devicemotion', deviceMotionHandler, false);
+if (window.DeviceOrientationEvent) {
+  document.getElementById("title").innerHTML = "Device Orientation";
+  // Listen for the deviceorientation event and handle the raw data
+  window.addEventListener('deviceorientation', function(eventData) {
+    // gamma is the left-to-right tilt in degrees, where right is positive
+    var tiltLR = Math.round(eventData.gamma)
+
+    // beta is the front-to-back tilt in degrees, where front is positive
+    var tiltFB = Math.round(eventData.beta)
+
+    // alpha is the compass direction the device is facing in degrees
+    var dir = Math.round(eventData.alpha)
+
+    // call our orientation event handler
+    deviceOrientationHandler(tiltLR, tiltFB, dir)
+  }, false);
 } else {
-  document.getElementById("dmEvent").innerHTML = "Sorry your device doesn't suppport device motion"
+  document.getElementById("doDirection").innerHTML = "Not supported."
 }
 
+function deviceOrientationHandler(tiltLR, tiltFB, dir){
+document.getElementById("doDirection").innerHTML = dir
 
-function deviceMotionHandler(eventData) {
-  var info, xyz = "[X, Y, Z]";
-
-  // Grab the acceleration from the results
-  var acceleration = eventData.acceleration;
-  info = xyz.replace("X", acceleration.x);
-  info = info.replace("Y", acceleration.y);
-  info = info.replace("Z", acceleration.z);
-  document.getElementById("moAccel").innerHTML = info;
-
-  // Grab the acceleration including gravity from the results
-  acceleration = eventData.accelerationIncludingGravity;
-  info = xyz.replace("X", acceleration.x);
-  info = info.replace("Y", acceleration.y);
-  info = info.replace("Z", acceleration.z);
-  document.getElementById("moAccelGrav").innerHTML = info;
-
-  // Grab the rotation rate from the results
-  var rotation = eventData.rotationRate;
-  info = xyz.replace("X", rotation.alpha);
-  info = info.replace("Y", rotation.beta);
-  info = info.replace("Z", rotation.gamma);
-  document.getElementById("moRotation").innerHTML = info;
-
-  // // Grab the refresh interval from the results
-  info = eventData.interval;
-  document.getElementById("moInterval").innerHTML = info;       
+if(dir > 90 && dir < 180){
+   document.getElementById("container").style.backgroundColor = "red"
+} else {
+    document.getElementById("container").style.backgroundColor = "white"
 }
+};
+
